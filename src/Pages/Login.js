b176a3b7a855b3f7f463baba7f947ed5835d8481
Victorcalwin <104540827+Victorcalwin@users.noreply.github.com>
2022-11-01 18:57:18 +0530

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -55,8 +55,7 @@ function handleLogin(e){
        <p className="text-center">
            Don't have an account yet ? <Link to="/signup">Signup</Link>
            </p>
-           <p>username:test,test1</p>
-           <p>password:123456</p>
+         
        </div>
 </Form>
 </Col>
